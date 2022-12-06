c841d3c256cbe3f43156540f84273fa7c629e4f1
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2022-12-07 00:27:28 +0600

    footer fix

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,3 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.0.3
-    </div>
-    <strong>Copyright &copy; 2014-2022.</strong> All rights
-    reserved.
+
 </footer>
